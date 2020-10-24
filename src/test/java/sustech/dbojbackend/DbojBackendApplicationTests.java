0a4ebdb597eca71d1bab4ee9ffa94178f829f4ca
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-10-25 00:39:18 +0800

    initial commit.
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ src/test/java/sustech/dbojbackend/DbojBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package sustech.dbojbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbojBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
