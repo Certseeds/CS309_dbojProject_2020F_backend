6a770a43d14801f4d1e2b0b8368b61d91cb81e3c
hotaru555 <1500241540@qq.com>
2020-11-19 07:49:52 -0800

    commit: login, signin and reset

--- src/main/java/sustech/dbojbackend/repository/UserRepository.java
+++ src/main/java/sustech/dbojbackend/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package sustech.dbojbackend.repository;
 
-import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
