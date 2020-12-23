3481282d29487d506a7c52859df5e630de5c4a12
hotaru555 <rngc1970>
2020-12-23 13:01:13 +0800

    add ./judgestatues api
    Signed-off-by: Certseeds <51754303+Certseeds@users.noreply.github.com>

---
+++ src/main/java/sustech/dbojbackend/model/response/Judgestatus.java
@@ -0,0 +1,23 @@
+package sustech.dbojbackend.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Judgestatus implements Serializable {
+    private static final long serialVersionUID = -4298842306672275881L;
+    private String user;
+    private String problem;
+    private String table;
+    private String result;
+    private Long time;
+    private Long memory;
+    private String language;
+    private Integer length;
+}
